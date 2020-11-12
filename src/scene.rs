9992a3ecffaf01abdab5e4db905f70399b4462f8
Nathan Graule <solarliner@gmail.com>
2020-11-12 23:43:15 +0100

    Add sun in the sky :)

--- src/scene.rs
+++ src/scene.rs
@@ -1,19 +1,16 @@
+use cgmath::{ElementWise, InnerSpace, Zero};
+use rand::{prelude::*, thread_rng, Rng};
+use rayon::prelude::*;
+use serde::Deserialize;
+
 use crate::{
     camera::{Camera, CameraParameters},
+    material::Bounce,
+    ray::Ray,
     sky::Sky,
     traits::Hittable,
-    Color,
-    ray::Ray,
-    material::Bounce,
+    Color, V3,
 };
-use rand::{
-    thread_rng,
-    Rng,
-    prelude::*,
-};
-use rayon::prelude::*;
-use cgmath::{ElementWise, Zero};
-use serde::Deserialize;
 
 #[derive(Copy, Clone, Debug, Deserialize)]
 pub struct Scene<W> {
@@ -26,22 +23,36 @@ pub struct Scene<W> {
 }
 
 impl<W: 'static + Hittable + Send> Scene<W> {
-    pub fn run(self, width: u32, height: u32) -> impl Iterator<Item=Vec<Color>> {
+    pub fn run(self, width: u32, height: u32) -> impl Iterator<Item = Vec<Color>> {
         let distr = rand::distributions::Uniform::new(0.0, 1.0);
         let (tx, rx) = crossbeam::channel::unbounded::<Vec<Color>>();
-        let cam = Camera::new(self.camera.look_from, self.camera.look_at, self.camera.up, width as f64 / height as f64, self.camera.fov, self.camera.aperture, self.camera.focus_distance);
+        let cam = Camera::new(
+            self.camera.look_from,
+            self.camera.look_at,
+            self.camera.up,
+            width as f64 / height as f64,
+            self.camera.fov,
+            self.camera.aperture,
+            self.camera.focus_distance,
+        );
 
         std::thread::spawn(move || {
             for j in (0..height).rev() {
-                let row: Vec<Color> = (0..width).into_par_iter().map(|i| {
-                    let mut rng = thread_rng();
-                     (0..self.samples).map(|_| {
-                         let u = (i as f64 + rng.sample(distr)) / (width-1) as f64;
-                         let v = (j as f64 + rng.sample(distr)) / (height-1) as f64;
-                         let ray = cam.get_ray(&mut rng, u, v);
-                         self.ray_color(&mut rng, &self.world, ray, self.bounces)
-                     }).sum::<Color>() / self.samples as f64
-                }).collect();
+                let row: Vec<Color> = (0..width)
+                    .into_par_iter()
+                    .map(|i| {
+                        let mut rng = thread_rng();
+                        (0..self.samples)
+                            .map(|_| {
+                                let u = (i as f64 + rng.sample(distr)) / (width - 1) as f64;
+                                let v = (j as f64 + rng.sample(distr)) / (height - 1) as f64;
+                                let ray = cam.get_ray(&mut rng, u, v);
+                                self.ray_color(&mut rng, ray, self.bounces)
+                            })
+                            .sum::<Color>()
+                            / self.samples as f64
+                    })
+                    .collect();
                 tx.send(row).unwrap();
             }
             std::mem::drop(tx);
@@ -49,17 +60,17 @@ impl<W: 'static + Hittable + Send> Scene<W> {
         rx.into_iter()
     }
 
-    fn ray_color(&self, rng: &mut ThreadRng, world: &W, ray: Ray, depth: u32) -> Color {
+    fn ray_color(&self, rng: &mut ThreadRng, ray: Ray, depth: u32) -> Color {
         if depth == 0 {
             Color::zero()
         } else {
-            if let Some(h) = world.hit(&ray, 0.001, std::f64::INFINITY) {
+            if let Some(h) = self.world.hit(&ray, 0.001, std::f64::INFINITY) {
                 match h.material.scatter(&ray, &h) {
                     Bounce::Bounce(color, ray) => {
                         if depth == 1 {
                             color
                         } else {
-                            let inner = self.ray_color(rng, world, ray, depth-1);
+                            let inner = self.ray_color(rng, ray, depth - 1);
                             color.mul_element_wise(inner)
                         }
                     }
--- src/sky.rs
+++ src/sky.rs
@@ -1,12 +1,20 @@
 use crate::{Color, V3};
 use serde::Deserialize;
+use cgmath::InnerSpace;
 
 #[derive(Copy, Clone, Debug, Default, Deserialize)]
 pub struct Sky;
 
 impl Sky {
     pub fn get_color(&self, dir: V3) -> Color {
-        let t = 0.5 * (dir.y + 1.0);
-        Color::from(V3::new(1.0, 1.0, 1.0) * (1.0 - t) + V3::new(0.5, 0.7, 1.0) * t)
+        let light_dir = V3::new(1.0, 1.0, 1.0).normalize();
+        let theta = dir.y.atan2(dir.x);
+        let phi = dir.z.acos(); // Assuming dir is constant
+        if dir.dot(light_dir) > 0.998 {
+            Color::new(100.0, 100.0, 100.0)
+        } else {
+            let t = 0.5 * (dir.y + 1.0);
+            Color::from(V3::new(1.0, 1.0, 1.0) * (1.0 - t) + V3::new(0.5, 0.7, 1.0) * t)
+        }
     }
 }
